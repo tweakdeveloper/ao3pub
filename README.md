e0398137e7eadecd51e9dae3f3d0d81651fb9b41
Nolan Clark <tweakdeveloper@gmail.com>
2022-02-13 11:51:31 -0500

    hello ao3pub

---
+++ README.md
@@ -0,0 +1,3 @@
+# ao3pub
+
+ao3pub helps to reformat works on ao3 into a format suitable for publication.
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", handleRoot)
+	r.Run()
+}
+
+func handleRoot(c *gin.Context) {
+	c.String(http.StatusOK, "howdy!")
+}
